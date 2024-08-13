c29a40de94b4eab121f8ce37ed3bdc7f7302bf6c
sergioBustamante <sergio.bustamante@niceincontact.com>
2024-08-13 09:57:00 -0400

    Todo anadido

--- src/presenter.js
+++ src/presenter.js
@@ -1,10 +1,13 @@
 import sumar from "./sumador";
-
+import multiplicar from "./multiplicador";
 const first = document.querySelector("#primer-numero");
 const second = document.querySelector("#segundo-numero");
+const third = document.querySelector("#tercer-numero");
+const fourth = document.querySelector("#cuarto-numero");
 const form = document.querySelector("#sumar-form");
+const multform = document.querySelector("#multiplicar-form");
 const div = document.querySelector("#resultado-div");
-
+const multdiv = document.querySelector("#resultadomult-div");
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
@@ -12,4 +15,12 @@ form.addEventListener("submit", (event) => {
   const secondNumber = Number.parseInt(second.value);
 
   div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+});
+multform.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const thirdNumber = Number.parseInt(third.value);
+  const fourthNumber = Number.parseInt(fourth.value);
+
+  multdiv.innerHTML = "<p>" + multiplicar(thirdNumber, fourthNumber) + "</p>";
 });
